d73d126b8425560e4676d9f63562464e14806839
GyuHyeokjjookki <jjookki33@gmail.com>
2023-09-22 14:27:09 +0900

    upload 4th assignment files

---
+++ assignment4/settingweb_boot/src/main/java/com/devfun/settingweb_boot/service/StatisticService.java
@@ -0,0 +1,9 @@
+package com.devfun.settingweb_boot.service;
+
+import java.util.HashMap;
+
+public interface StatisticService {
+    public HashMap<String,Object> yearloginNum (String year);
+
+    public HashMap<String,Object> monthloginNum(String month);
+}
---
+++ assignment4/settingweb_boot/src/main/java/com/devfun/settingweb_boot/service/StatisticServiceImpl.java
@@ -0,0 +1,52 @@
+package com.devfun.settingweb_boot.service;
+
+
+import java.util.HashMap;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devfun.settingweb_boot.dao.StatisticMapper;
+
+@Service
+public class StatisticServiceImpl implements StatisticService {
+
+
+    @Autowired
+    private StatisticMapper uMapper;
+
+    @Override
+    public HashMap<String, Object> yearloginNum (String year) {
+        // TODO Auto-generated method stub
+        HashMap<String, Object> retVal = new HashMap<String,Object>();
+
+        try {
+            retVal = uMapper.selectYearLogin(year);
+            retVal.put("year", year);
+            retVal.put("is_success", true);
+
+        }catch(Exception e) {
+            retVal.put("totCnt", -999);
+            retVal.put("year", year);
+            retVal.put("is_success", false);
+        }
+
+        return retVal;
+    }
+    @Override
+    public HashMap<String, Object> monthloginNum(String month){
+        HashMap<String, Object> retVal = new HashMap<String,Object>();
+
+        try {
+            retVal = uMapper.selectMonthLogin(month);
+            retVal.put("month", month);
+            retVal.put("is_success", true);
+
+        }catch(Exception e) {
+            retVal.put("totCnt", -999);
+            retVal.put("month", month);
+            retVal.put("is_success", false);
+        }
+
+        return retVal;
+    }
+}
